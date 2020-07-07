817b8d7c29dfa306d818ad85a39422e658e74c82
Luiz Henrique Sanches Serafim <a29061@somoseducacao.com.br>
2020-07-07 07:32:08 -0300

    Projeto Criado

---
+++ src/br/com/alvoradatec/calculadora/Calculadora.java
@@ -0,0 +1,16 @@
+package br.com.alvoradatec.calculadora;
+
+import javax.swing.*;
+
+public class Calculadora extends JFrame {
+    public Calculadora() {
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE); // poderia ser DISPOSE_ON_CLOSE
+        setSize(232,322);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        new Calculadora();
+    }
+}
